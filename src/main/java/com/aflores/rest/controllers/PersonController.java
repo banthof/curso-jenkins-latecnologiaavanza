2475896f47dfd50e9c45c04375d556f30117f02f
Anthony Aldair Flores Berrocal <anthony200699@hotmail.com>
2025-06-03 08:22:14 -0500

    add test with junit and mockito - 1h 20m

--- src/main/java/com/aflores/rest/controllers/PersonController.java
+++ src/main/java/com/aflores/rest/controllers/PersonController.java
@@ -4,8 +4,6 @@ import com.aflores.rest.entities.Person;
 import com.aflores.rest.services.PersonService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
---
+++ src/test/java/com/aflores/rest/PersonControllerTest.java
@@ -0,0 +1,66 @@
+package com.aflores.rest;
+
+import com.aflores.rest.controllers.PersonController;
+import com.aflores.rest.entities.Person;
+import com.aflores.rest.services.PersonService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+public class PersonControllerTest {
+
+    @InjectMocks
+    private PersonController personController;
+
+    @Mock
+    private PersonService personService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void getAllPersons_WhenCalled_ReturnsListOfPersons() {
+        // Arrange
+        List<Person> mockPersons = Arrays.asList(
+                new Person(1, "Ana", "García", "ana@example.com"),
+                new Person(2, "Luis", "Martínez", "luis@example.com")
+        );
+        when(personService.getPersons()).thenReturn(mockPersons);
+
+        // Act
+        ResponseEntity<?> response = personController.getAllPersons();
+
+        // Assert
+        assertEquals(200, response.getStatusCode().value());
+        assertNotNull(response.getBody());
+        assertEquals(mockPersons, response.getBody());
+    }
+
+    @Test
+    void getPersonById_WithValidId_ReturnsPerson() {
+        // Arrange
+        Integer id = 1;
+        Person mockPerson = new Person(id, "Ana", "García", "ana@example.com");
+        when(personService.getById(id)).thenReturn(mockPerson);
+
+        // Act
+        ResponseEntity<?> response = personController.getPersonById(id);
+
+        // Assert
+        assertEquals(200, response.getStatusCode().value());
+        assertNotNull(response.getBody());
+        assertEquals(mockPerson, response.getBody());
+    }
+
+}
